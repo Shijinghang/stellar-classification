3daee268b414ac4f7be7bcec3481228de529b1fa
Shijinghang <63154972+Shijinghang@users.noreply.github.com>
2023-09-19 15:25:18 +0800

    second

--- predict.py
+++ predict.py
@@ -10,18 +10,18 @@ from utlis.datasets import MyDataset
 from utlis.model import SCNet
 
 if __name__ == '__main__':
-    # 参数设置
+    # Parameter settings
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='config/dataset.yaml', help='dataset config file path')
     parser.add_argument('--hyp', type=str, default='config/hyp.yaml', help='hyperparameters path')
 
     parser.add_argument('--output-path', type=str, default='result/', help='output result path')
-    parser.add_argument('--weights-path', type=str, default='result/best.pth')
+    parser.add_argument('--weights-path', type=str, default='result/SCNet_gri.pth')
     parser.add_argument('--img-size', nargs='+', type=int, default=[64, 64], help='[wide, height]')
     parser.add_argument('--batch-size', type=int, default=128, help='total batch size for all GPUs')
     opt = parser.parse_args()
 
-    # 数据集信息加载
+    # Dataset information loading
     with open(opt.data, encoding='utf-8') as f:
         data_dict = yaml.load(f, Loader=yaml.FullLoader)  # data dict
 
@@ -32,12 +32,12 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # 获取数据
+    # get data
     test_path = data_dict['test']
     test_files = get_file_paths(test_path)
     test_dataset = MyDataset((test_files, 'folder'), names=data_dict['names'], hyp_dict=hyp_dict)
     test_loader = dataloader = DataLoader(dataset=test_dataset, num_workers=2, batch_size=opt.batch_size)
-    # 加载模型
+    # loading model
     net = SCNet(nc=data_dict['nc']).eval()
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     net.load_state_dict(state_dict, strict=False)
     net = net.cuda()
 
-    # 预测
+    # prediction
     y_pred, y_ture = [], []
     for iteration, batch in tqdm(enumerate(test_loader)):
         datas, labels = batch[0], batch[1]
@@ -56,14 +56,14 @@ if __name__ == '__main__':
         y_pred += outputs.argmax(dim=1).cpu()
         y_ture += labels.cpu()
 
-    # 计算各项指标并保存
+    # Calculate various indicators and save them
     os.makedirs(os.path.join(opt.output_path, 'test'), exist_ok=True)
     result = classification_report(y_ture, y_pred, digits=4, target_names=data_dict['names'])
     with open(f'{opt.output_path}/test/metric.txt', 'w') as f:
         f.write(result)
     print(result)
 
-    # 计算混淆矩阵，并绘图保存
+    # Calculate the confusion matrix and save the drawing
     cm = confusion_matrix(y_ture, y_pred)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=data_dict['names'])
     fig, ax = plt.subplots(dpi=300)
---
+++ result/README.md
@@ -0,0 +1,4 @@
+Due to Github's limitation on the size of file uploads, we are unable to fully upload the stellar-type catalog and training model to Github.
+
+But you can obtain a complete stellar-type catalog through https://nadc.china-vo.org/res/r101125/,
+and the weights of our trained SCNet and SCNet gri through https://drive.google.com/drive/folders/1_nZfjXI27ekGM1nZCOEqa95yVmASMZEX?usp=drive_link
